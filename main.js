5ef9787ddf6074bce351a19259a8422cff48a085
Pavel Tsvetkov <paveltsvetkov2005@gmail.com>
2025-07-14 12:45:36 +0300

    Good Evevning! Have a good day!

---
+++ main.js
@@ -0,0 +1,45 @@
+var attack_mode=true
+
+setInterval(
+	function(){
+
+	if (character.hp < 1300) {
+		use_skill('use_hp');
+	}
+		
+	if (character.mp < 100) {
+		use_skill('use_mp');
+	}
+		
+	loot();
+
+	if(!attack_mode || character.rip || is_moving(character)) return;
+
+	var target=get_targeted_monster();
+	if(!target)
+	{
+		target=get_nearest_monster({min_xp:100,max_att:120});
+		if(target) change_target(target);
+		else
+		{
+			set_message("No Monsters");
+			return;
+		}
+	}
+	
+	if(!is_in_range(target))
+	{
+		move(
+			character.x+(target.x-character.x)/2,
+			character.y+(target.y-character.y)/2
+			);
+		// Walk half the distance
+	}
+	else if(can_attack(target))
+	{
+		set_message("Attacking");
+		attack(target);
+	}
+
+}
+	,250); 
